5141ffb41a48953bb98af7b47d09836a8928be23
Jeremiah Tria <triajeremiah@gmail.com>
2021-06-23 14:49:01 +1200

    Uploading New Files
    Created a Visual Studio Project
    Created MainClass and Ticket Class

---
+++ IT5014_Project_20210603/MainClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT5014_Project_20210603
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WOW");
+
+            Ticket problem1 = new Ticket("John", "john@whitecliffe.co.nz", "Password change", "JOHNS");
+            Ticket problem2 = new Ticket("Inna", "inna@whitecliffe.co.nz", "My monitor stopped working", "INNAM");
+            Ticket problem3 = new Ticket("Maria", "maria@whitecliffe.co.nz", "Request for a video camera to conduct webinars", "MARIAH");
+
+            problem1.GenerateTicket();
+
+        }
+    }
+}
---
+++ IT5014_Project_20210603/Ticket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT5014_Project_20210603
+{
+    class Ticket
+    {
+        static int tk_Num = 2000;   // Automatically set to 2000
+        string tk_Creator;          // Name of the creator of ticket
+        string tk_Email;            // Contact email of the creator of ticket
+        string tk_Desc;             // Description of issue
+        string tk_StaffID;          // StaffID of the creator of ticket
+        string tk_Response;         // Automatically set "Not Yet Opened" when ticket is first created
+        string tk_Status;           // Automatically set to Open once ticket is created
+
+        public Ticket(string name, string email, string desc, string staffID)
+        {
+            tk_Num++;
+            this.tk_Creator = name;
+            this.tk_Email = email;
+            this.tk_Desc = desc;
+            this.tk_StaffID = staffID;
+            tk_Response = "Not Yet Provided";
+            tk_Status = "Open";
+
+        }
+
+        public string GenerateTicket()
+        {
+            return tk_Creator;
+        }
+
+    }
+}
