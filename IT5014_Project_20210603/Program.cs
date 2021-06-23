2139b8cddc5f652baaf61030a30b4af11b053aff
Jeremiah Tria <triajeremiah@gmail.com>
2021-06-23 14:37:00 +1200

    Add project files.

---
+++ IT5014_Project_20210603/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT5014_Project_20210603
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
